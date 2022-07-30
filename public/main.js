6dd8eea97ff4bec3187e79bf72701cbfd8c4eff3
Beth Taiwo <elizabeth@walulel.com>
2022-07-30 21:40:55 +0000

    ts setup

---
+++ public/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Language: typescript
+// Path: main.ts
+var names = ["Beth", "Choongsaeng", "Sonia", "Sophie", "Stephen"];
+console.log(names);
+let circ = (diameter) => {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
+console.log("Holaa carino");
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// Language: typescript
+// Path: main.ts
+var names = ["Beth", "Choongsaeng", "Sonia", "Sophie", "Stephen"];
+console.log(names);
+
+let circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(10));
+
+console.log("Holaa carino");
