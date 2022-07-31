4c17ac2cf4751ba51d43a309d643445689585956
Beth Taiwo <elizabeth@walulel.com>
2022-07-31 20:07:00 +0000

    playing around with function signature

--- public/main.js
+++ public/main.js
@@ -13,3 +13,21 @@ const greetAgain = (user) => {
 logDetails(123, "car");
 greet({ firstName: "Max", lastName: "Smith" });
 greetAgain({ firstName: "Jane", lastName: "Doe" });
+// function signature
+let calc;
+calc = (numOne, numTwo, operator) => {
+    if (operator === "+") {
+        console.log(numOne + numTwo);
+        return numOne + numTwo;
+    }
+    else {
+        console.log(numOne - numTwo);
+        return numOne - numTwo;
+    }
+};
+calc(2, 3, "-");
+let printDetails;
+printDetails = (ninja) => {
+    console.log(`${ninja.name} is not up to ${ninja.age} years old`);
+};
+printDetails({ name: "Beth", age: 30 });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // Language: typescript
 // Path: main.ts
 
+// type aliases
 type StringOrNum = string | number;
 type User = { firstName: string; lastName: string };
 
@@ -19,3 +20,26 @@ const greetAgain = (user: User) => {
 logDetails(123, "car");
 greet({ firstName: "Max", lastName: "Smith" });
 greetAgain({ firstName: "Jane", lastName: "Doe" });
+
+// function signature
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, operator: string) => {
+  if (operator === "+") {
+    console.log(numOne + numTwo);
+    return numOne + numTwo;
+  } else {
+    console.log(numOne - numTwo);
+    return numOne - numTwo;
+  }
+};
+calc(2, 3, "-");
+
+let printDetails: (obj: { name: string; age: number }) => void;
+type person = { name: string; age: number };
+
+printDetails = (ninja: person) => {
+  console.log(`${ninja.name} is not up to ${ninja.age} years old`);
+};
+
+printDetails({ name: "Beth", age: 30 });
